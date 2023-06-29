f811567b0135ad4fd42164f2bc69bb56b5244be0
Thiago Fernando <thiago.fernando@msn.com>
2023-06-29 14:55:13 -0400

    Day 1

---
+++ DevHub/app/src/main/java/br/com/alura/devhub/MainActivity.kt
@@ -0,0 +1,57 @@
+package br.com.alura.devhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.alura.devhub.ui.theme.DevHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DevHubTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ExibirImagem()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExibirImagem(){
+    Column() {
+            Image(
+                painter = painterResource(id = R.drawable.naruto),
+                contentDescription = stringResource(R.string.foto_perfil),
+            );
+            Column() {
+                Text(text = "Thiago");
+                Text(text = "thiiagofernando");
+                Text(text = "Desenvolvedor de Software");
+            }
+    }
+}
