e99eb6c0b53ebbca84c33cf337e9413ad11768ff
Thiago Fernando <thiago.fernando@msn.com>
2023-07-04 13:24:50 -0400

    Day 2

--- DevHub/app/src/main/java/br/com/alura/devhub/MainActivity.kt
+++ DevHub/app/src/main/java/br/com/alura/devhub/MainActivity.kt
@@ -5,22 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alura.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,18 +44,55 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+
 @Composable
 fun ExibirImagem(){
     Column() {
+        val boxHeight = remember {
+            150.dp
+        }
+        val imageHeight = remember{
+            boxHeight
+        }
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                               .background(
+                                   Color(0xFF2d333b), shape =  RoundedCornerShape(
+                                       bottomStart = 16.dp,
+                                       bottomEnd = 16.dp
+                                   )
+                               ).height(boxHeight)
+        ){
             Image(
                 painter = painterResource(id = R.drawable.naruto),
                 contentDescription = stringResource(R.string.foto_perfil),
-            );
-            Column() {
-                Text(text = "Thiago");
-                Text(text = "thiiagofernando");
-                Text(text = "Desenvolvedor de Software");
+                modifier =  Modifier
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(CircleShape),
+            )
+        }
+        Spacer(modifier = Modifier.height(imageHeight / 2))
+            Column(
+                Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Thiago",fontSize = 32.sp);
+                Text(text = "thiiagofernando",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold);
+                Text(text = "Desenvolvedor de Software",
+                    Modifier
+                        .padding(
+                            start = 8.dp,
+                            bottom = 8.dp,
+                            end = 8.dp
+                        )
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center);
             }
     }
 }
